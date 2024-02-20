34f72fd6b32d3ccf4b64cf05ac8d968077df015b
okuraayno <ohsushi555@gmail.com>
2024-02-20 11:38:55 +0000

    clear

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get '/homes/about', to: 'homes#about', as: 'about'
+  get '/home/about', to: 'homes#about', as: 'about'
+  get '/users/:id', to: 'users#show', as: 'user'
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :index, :edit, :update]
 end
